cd8820165b5e3a7c0406495581606917f030075c
mohamadyaserakbari <50046855+mohamadyaserakbari@users.noreply.github.com>
2022-12-15 21:38:14 +0330

    adding Reset button

--- src/App.js
+++ src/App.js
@@ -1,22 +1,72 @@
 import React, { Component } from 'react';
 import './App.css';
 import {
+  Button,
   Container, Navbar,
 } from 'reactstrap';
 import NavBar from './components/navbar';
 import Counters from './components/counters';
 
-function App() {
-  return (
-    <React.Fragment>
-      <NavBar />
-      <Container>
-        <div style={{ float: "right" }}>
-          <Counters />
-        </div>
-      </Container>
-    </React.Fragment>
-  );
-}
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+      { id: 5, value: 0 }
+    ]
+  }
 
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  }
+
+  handleDecrease = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    if (counters[index].value > 0) {
+      counters[index].value--;
+    } else {
+      counters[index].value = 0;
+    }
+    this.setState({ counters });
+  }
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  }
+
+  handelReset = () => {
+    const counters = this.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <Container>
+          <div style={{ float: "right" }}>
+            <Counters>
+              counters = {this.state.counters}
+              onIncrement = {this.handleIncrement}
+              onDecrease = {this.handleDecrease}
+              onDelete = {this.handleDelete}
+            </Counters>
+            <Button color='success' className='btn-sm m-2'>تنظیم مجدد</Button>
+          </div>
+        </Container>
+      </React.Fragment>
+    );
+  }
+}
 export default App;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,61 +1,26 @@
 import React, { Component } from 'react';
 import Counter from './counter';
-import {
-    Button,
-    Container
-} from 'reactstrap';
 
 class Counters extends Component {
-    state = {
-        counters: [
-            { id: 1, value: 0 },
-            { id: 2, value: 0 },
-            { id: 3, value: 0 },
-            { id: 4, value: 0 },
-            { id: 5, value: 0 }
-        ]
-    }
-
-    handleIncrement = counter => {
-        const counters = [...this.state.counters];
-        const index = counters.indexOf(counter);
-        counters[index] = { ...counter };
-        counters[index].value++;
-        this.setState({ counters });
-    }
-
-    handleDecrease = counter => {
-        const counters = [...this.state.counters];
-        const index = counters.indexOf(counter);
-        counters[index] = { ...counter };
-        if (counters[index].value > 0) {
-            counters[index].value--;
-        } else {
-            counters[index].value = 0;
-        }
-        this.setState({ counters });
-    }
-
-    handleDelete = (counterId) => {
-        const counters = this.state.counters.filter(c => c.id !== counterId);
-        this.setState({ counters });
-    }
-
     render() {
+        const {
+            onDelete,
+            onDecrease,
+            onIncrement,
+            counters
+        } = this.props();
         return (
-            <Container>
-                <div style={{ float: "right" }}>
-                    {this.state.counters.map(counter =>
-                        <Counter
-                            key={counter.id}
-                            counter={counter}
-                            onIncrement={this.handleIncrement}
-                            onDecrease={this.handleDecrease}
-                            onDelete={this.handleDelete}
-                        />
-                    )}
-                </div>
-            </Container>
+            <div style={{ float: "right" }}>
+                {counters.map(counter =>
+                    <Counter
+                        key={counter.id}
+                        counter={counter}
+                        onIncrement={onIncrement}
+                        onDecrease={onDecrease}
+                        onDelete={onDelete}
+                    />
+                )}
+            </div>
         );
     }
 }
